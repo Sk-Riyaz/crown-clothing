de8b4597c5738cef049bd93126d9a7e461cda865
Sk-Riyaz <skriyaz09@gmail.com>
2019-09-05 00:20:48 +0530

    Added header to the app and routing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
